2bbb1721379fd73f6d4a6acbc92156a9e92d75d0
Brandon Wharton <whartonbm@gmail.com>
2021-06-26 11:04:57 -0500

    Made an AddressList component"

---
+++ src/AddressList/AddressList.jsx
@@ -0,0 +1,14 @@
+import Typography from '@material-ui/core/Typography';
+
+function AddressList() {
+
+    return (
+        <div className="App-address-list">
+            <Typography variant="h3" component="h3">
+                Inside AddressList
+            </Typography>
+        </div>
+    )
+}
+
+export default AddressList;
--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from '../Header/Header';
+import AddressList from '../AddressList/AddressList';
 import Footer from '../Footer/Footer';
 import '@fontsource/roboto';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AddressList />
       <Footer />
     </div>
   );
