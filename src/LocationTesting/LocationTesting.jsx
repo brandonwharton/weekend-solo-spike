7fd4bb0444f53e84fc1d2612a7b1f8c0992fabbb
Brandon Wharton <whartonbm@gmail.com>
2021-06-27 14:15:19 -0500

    Made a change handler and an input for the first address

--- src/LocationTesting/LocationTesting.jsx
+++ src/LocationTesting/LocationTesting.jsx
@@ -1,14 +1,48 @@
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+
+import './LocationTesting.css';
 
 function LocationTesting() {
     // state for storing current location after finding it
     const [currentLocation, setCurrentLocation] = useState({});
+    // states for storing address input data
+    const [firstAddress, setFirstAddress] = useState({
+        number: 0,
+        street_name: '',
+        city: '',
+        state_code: ''
+    });
+    const [secondAddress, setSecondAddress] = useState({
+        number: 0,
+        street_name: '',
+        city: '',
+        state_code: ''
+    });
+
 
     useEffect(() => {
         getLocation();
     }, []);
+
+    // change handlers for the two address inputs
+    const handleFirstAddressChange = (event, propertyName) => {
+        setFirstAddress({
+            ...firstAddress,
+            [propertyName]: event.target.value
+        })
+    }
+
+    const handleSecondAddressChange = (event, propertyName) => {
+        setSecondAddress({
+            ...secondAddress,
+            [propertyName]: event.target.value
+        })
+    }
+
 
     // makes a request to retrieve and store user's current location using Google Maps APIs
     const getLocation = () => {
@@ -30,7 +64,12 @@ function LocationTesting() {
             })
     }
 
-    console.log('local location state', currentLocation);
+    const sendAddressInfo = () => {
+        console.log('clicked');
+    }
+
+    // console.log('local location state', currentLocation);
+    console.log('Current address object', firstAddress);
     return (
         <div className="App-location-testing">
             <Button 
@@ -40,6 +79,30 @@ function LocationTesting() {
             >
                 Get Current Location
             </Button>
+            <FormControl className="address-form">
+                <TextField 
+                    label="first address number"
+                    onChange={(event) => handleFirstAddressChange(event, 'number')}
+                >
+                </TextField>
+                <TextField 
+                    label="street name"
+                    onChange={(event) => handleFirstAddressChange(event, 'street_name')}
+                >
+                </TextField>
+                <TextField 
+                    label="city"
+                    onChange={(event) => handleFirstAddressChange(event, 'city')}
+                >
+                </TextField>
+                <TextField 
+                    label="state"
+                    onChange={(event) => handleFirstAddressChange(event, 'state_code')}
+                >
+                </TextField>
+            </FormControl>
+
+            
         </div>
 
     )
